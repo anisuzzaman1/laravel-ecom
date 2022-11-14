d7cd80d1557aed0bf9bbad029638bed2d6d6a1de
Md Anisuzzaman <anisuzzzaman2112@gmail.com>
2022-11-15 01:27:27 +0600

    Create table and Model for Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $table = 'products';
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'brand',
+        'small_description',
+        'description',
+        'original_price',
+        'selling_price',
+        'quantity',
+        'trending',
+        'status',
+        'meta_title',
+        'meta_keyword',
+        'meta_description',
+    ];
 }
